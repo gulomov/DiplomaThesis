ab7f5f479655968997b5122f18fff3a8896224ae
Jaloliddin Gulomov <jaloliddin.gulomov@adastra.one>
2023-10-23 17:47:29 +0200

    Refactor modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,8 +16,6 @@ dependencyResolutionManagement {
 rootProject.name = "DiplomaThesis"
 include(":app")
 include(":features")
-include(":screens")
-include(":spash")
 include(":features:splash")
 include(":features:introduction")
 include(":features:home")
