be0a8b9428463c974b68b5f07578c594ab0487ad
Jaloliddin Gulomov <jaloliddin.gulomov@adastra.one>
2024-03-28 15:22:55 +0100

    Create new file for dimen

---
+++ core/design/src/main/java/com/diploma/work/design/theme/Dimen.kt
@@ -0,0 +1,29 @@
+package com.diploma.work.design.theme
+
+import androidx.compose.ui.unit.dp
+
+
+val small50 = 4.dp
+val small100 = 8.dp
+val small150 = 12.dp
+
+val normal100 = 16.dp
+val normal150 = 24.dp
+
+val large100 = 32.dp
+val large125 = 40.dp
+val large150 = 48.dp
+val large200 = 64.dp
+
+val cornerNormal25 = 4.dp
+val cornerNormal100 = 16.dp
+val cornerNormal150 = 24.dp
+val cornerNormal175 = 28.dp
+
+val elevationNormal100 = 12.dp
+
+val dividerNormal100 = 1.dp
+
+val singleLineItemHeight = 56.dp
+
+val iconSize = 24.dp
