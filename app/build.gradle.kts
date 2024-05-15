8caa6df084887fafd6f3b2c6269f8fc79be97109
Jaloliddin Gulomov <jaloliddin.gulomov@adastra.one>
2024-05-15 12:42:55 +0200

    Update version name to 0.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         compileSdk = DefaultConfig.compileSdkVersion
         targetSdk = DefaultConfig.targetSdkVersion
         versionCode = 1
-        versionName = "0.0.1"
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
