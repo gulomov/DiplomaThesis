889b13758777435c00e9f0d6a3d0fea17f008c0a
gulomov <jalolidding98@gmail.com>
2024-08-06 10:36:27 +0200

    Finish refactoring view model and ui

--- features/home/src/main/java/com/diploma/work/home/recommendations/RecommendationsDetail.kt
+++ features/home/src/main/java/com/diploma/work/home/recommendations/RecommendationsDetail.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
