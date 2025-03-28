55bdc64eb2d714eb7ee8c36a9454e4e5054b4009
jaloliddingulomov <jaloliddin.gulomov@adastragrp.com>
2025-03-28 14:16:02 +0100

    Move navigation when product click into view model

--- features/favorites/src/main/java/com/diploma/work/favorites/FavoritesViewModel.kt
+++ features/favorites/src/main/java/com/diploma/work/favorites/FavoritesViewModel.kt
@@ -4,10 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.diploma.work.common.domain.DeleteFromFavoriteProductsUseCase
 import com.diploma.work.favorites.domain.GetFavoriteProductsUseCase
-import com.diploma.work.repository.data.FavoriteProduct
+import com.diploma.work.navigation.ScreenRoute.PRODUCTION_DETAIL
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,17 +16,40 @@ class FavoritesViewModel @Inject constructor(
     getFavoriteProductsUseCase: GetFavoriteProductsUseCase,
     private val deleteFromFavoriteProductsUseCase: DeleteFromFavoriteProductsUseCase
 ) : ViewModel() {
-    val uiState = MutableStateFlow(FavoriteScreenState())
+    val uiState = MutableStateFlow(FavoriteScreenUiState())
+
+    private val navigateRoute = MutableStateFlow<String?>(null)
 
     init {
         viewModelScope.launch {
+            combine(
+                getFavoriteProductsUseCase(),
+                navigateRoute
+            ) { favoriteProducts, navigateRoute ->
+                FavoriteScreenUiState(
+                    favoriteProducts = favoriteProducts,
+                    navigateRoute = navigateRoute
+                )
+            }.collect {
+                uiState.value = it
+            }
+
             getFavoriteProductsUseCase().collect {
-                uiState.value = FavoriteScreenState(
+                uiState.value = FavoriteScreenUiState(
                     favoriteProducts = it,
                     loadingValue = false
                 )
             }
         }
+    }
+
+    fun onProductClicked(productId: Int) {
+        val route = PRODUCTION_DETAIL.replace("{productId}", productId.toString())
+        navigateRoute.value = route
+    }
+
+    fun resetNavigateRoute() {
+        navigateRoute.value = null
     }
 
     fun deleteFromFavoriteProducts(productId: Int) = viewModelScope.launch {
